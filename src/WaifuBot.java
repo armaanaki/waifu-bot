8a5ffcab5d62943382dc5d1f23beb2474af29cd6
Armaan <armaanaki@gmail.com>
2018-07-16 14:38:46 -0400

    ability to store directories and their files in hashmap gitignore updates
    Signed-off-by: Armaan <armaanaki@gmail.com>

--- src/WaifuBot.java
+++ src/WaifuBot.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.HashMap;
 
 public class WaifuBot {
+	
+	//hashmap to store directories and files inside it
+	private static HashMap<String, File[]> waifuMap = new HashMap<String, File[]>();
+	
+	//filter for images only
+	private final static FilenameFilter IMAGES = new FilenameFilter() {
+		public boolean accept(File dir, String name) {
+				String[] extensions = {"gif", "png", "jpg", "bmp"};
+				for (String ext : extensions) 
+					if (name.endsWith(ext)) { return true; }
+				return false;
+		}
+	};
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			File waifusFolder = new File("./waifus");
+			File[] waifus = waifusFolder.listFiles(File::isDirectory);
+			
+			for (File waifu : waifus) 
+				waifuMap.put(waifu.getName(), waifu.listFiles(IMAGES));
+			
+			System.out.println(waifuMap.get("mio")[0]);
+			
 	}
 
 }
